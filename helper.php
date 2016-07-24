ea99b743a1a730f736f26bf0c18b80cfa0384d3f
Ben <tsun424@163.com>
2016-07-24 20:14:09 +1200

    adding season count function

--- helper.php
+++ helper.php
@@ -28,3 +28,23 @@ $testArr = [
 
 convertCamelKey($testArr);
 var_dump($testArr);
+
+
+function countSeason($endTime = "now"){
+	$returnArr = [];
+	$timeUnix = strtotime($endTime);
+	$season = ceil(date('n', $timeUnix)/3);
+	$firstTimeUnix = mktime(0, 0, 0,$season*3-2,1,date('Y', strtotime("-2 year", $timeUnix)));
+	$endDateUnix = mktime(23, 59, 59,$season*3,1,date('Y', $timeUnix));
+	$nextMonthUnix = $firstTimeUnix;
+	while ($nextMonthUnix < $endDateUnix) {
+		$tempStart = date('Y-m-01', $nextMonthUnix);
+		$tempEnd = date('Y-m-t', strtotime("+2 month", $nextMonthUnix));
+		$key = $tempStart."~".$tempEnd;
+		$value = date('M Y', $nextMonthUnix)."~".date('M Y', strtotime("+2 month", $nextMonthUnix));
+		$returnArr[$key] = $value;
+		$nextMonthUnix = strtotime("+3 month", $nextMonthUnix);
+	}
+	return $returnArr;
+}
+var_dump(countSeason("2016-11-01"));
